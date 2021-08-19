56815d89620d9a08da1f6e7ea903fa5a9454cdac
cod-lvr <aymang29@yahoo.com>
2021-08-20 01:20:20 +0200

    add animation with framer-motion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import {  AnimatePresence } from "framer-motion";
 
 import Home from "./pages/Home";
 import Store from "./pages/Store";
@@ -8,25 +8,29 @@ import NotfoundPage from "./pages/NotFoundPage";
 import Layout from "./components/UI/Layout";
 
 function App() {
+  const location = useLocation();
+
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to='/home'/>
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/store" exact>
-          <Store />
-        </Route>
-        <Route path="/store/:bookId">
-          <BookDetail />
-        </Route>
-        <Route path="*">
-          <NotfoundPage />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.key}>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/store" exact>
+            <Store />
+          </Route>
+          <Route path="/store/:bookId">
+            <BookDetail />
+          </Route>
+          <Route path="*">
+            <NotfoundPage />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </Layout>
   );
 }
--- src/components/layout/Books.js
+++ src/components/layout/Books.js
@@ -1,17 +1,18 @@
 import { Fragment } from "react";
 import Button from "../UI/Button";
+import classes from './Books.module.css';
 
 const Books = () => {
   return (
     <Fragment>
-      <div>
+      <div className={classes.card}>
         <h3>you can read buy anytime anywhere</h3>
         <h5>alot of free and premium books</h5>
         <Button>read now</Button>
       </div>
-      <div>
-        <div>
-          <label htmlFor="grens">grens :</label>
+      <div className={classes.filterbar}>
+        <div className={classes.controls}>
+          <label htmlFor="grens">grens: </label>
           <select name="grens" id="grens">
             <option value="history">history</option>
             <option value="socity">socity</option>
@@ -19,8 +20,8 @@ const Books = () => {
             <option value="Health/fitness">Health/fitness</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="filter">filter :</label>
+        <div className={classes.controls}>
+          <label htmlFor="filter">filter: </label>
           <select name="filter" id="filter">
             <option value="author">author</option>
             <option value="paragraph">paragraph</option>
