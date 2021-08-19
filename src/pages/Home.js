ea87630b212690c26e1b1a3eee70338f1caa7761
cod-lvr <aymang29@yahoo.com>
2021-08-20 01:23:31 +0200

    add static layout component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
+import { motion } from 'framer-motion';
 import HomePage from '../components/layout/HomePage';
 
+const homeVarient = {
+   visable: {
+       opacity: 1
+    },
+   hidden: {
+        opacity: 0
+   },
+   exit: {
+        opacity: 0,
+        x: "-100vw",
+        transition: { duration: 1 },
+   }
+}
+
 const Home = () => {
-    return <HomePage />
+    return (
+        <motion.div variants={homeVarient} exit="exit">
+            <HomePage />
+        </motion.div>
+    )
+    
 }
 
 export default Home;
