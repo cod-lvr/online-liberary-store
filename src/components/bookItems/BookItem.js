cd20575abcc5a4594494161b0ce50a4a9f4f6ff0
cod-lvr <aymang29@yahoo.com>
2021-08-21 04:21:39 +0200

    add BookItem component

--- src/components/bookItems/BookItem.js
+++ src/components/bookItems/BookItem.js
@@ -1,3 +1,43 @@
-const Bookitem = () => {};
+import React from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import classes from './BookItem.module.css';
+
+const Bookitem = () => {
+    return (
+        <div >
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { duration: 0.15 } }}
+                transition={{ duration: 0.2, delay: 0.15 }}
+                style={{ pointerEvents: "auto" }} 
+                className={classes.overlay} 
+            >
+                <Link to="/store/${book-name}" />
+            </motion.div>
+            <div className={classes["card-content-container open"]}>
+                <motion.div className={classes["card-content"]} layoutId={`card-container-${id}`}>
+                    <motion.div
+                        className={classes["card-image-container"]}
+                        layoutId={`card-image-container-${id}`}
+                    >
+                        <img className={classes["card-image"]} src={`images/${id}.jpg`} alt="" />
+                    </motion.div>
+                <motion.div
+                    className={classes["title-container"]}
+                    layoutId={`title-container-${id}`}
+                >
+                    <span></span>
+                    <h2></h2>
+                </motion.div>
+                    <motion.div className={classes["content-container"]} animate>
+                       
+                    </motion.div>
+                </motion.div>
+            </div>
+        </div>
+    )
+};
 
 export default Bookitem;
